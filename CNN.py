765854e23570b5ececcf5bda2c9a28db5e7317b7
ajax2411 <arushanjeyaganesh@gmail.com>
2022-12-25 20:37:00 -0500

    Organized Code

--- CNN.py
+++ CNN.py
@@ -1,6 +1,5 @@
 
 import torch.nn as nn
-from torch import flatten
 from torch.nn.functional import relu
 
 
@@ -11,10 +10,9 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
 
         self.conv1 = nn.Conv2d(in_channels, 10, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.conv2 = nn.Conv2d(10, 24, kernel_size=kernel_size, stride=stride)
 
         self.pool = nn.MaxPool2d(kernel_size, stride=stride)
-
-        self.conv2 = nn.Conv2d(10, 24, kernel_size=kernel_size, stride=stride)
 
         self.fc1 = nn.Linear(24 * kernel_size * kernel_size, 28)
         self.fc2 = nn.Linear(28, classes)
@@ -23,8 +21,6 @@ class CNN(nn.Module):
 
         x = self.pool(relu(self.conv1(x)))
         x = self.pool(relu(self.conv2(x)))
-
-        x = flatten(x, 1)
 
         x = relu(self.fc1(x))
         x = self.fc2(x)
